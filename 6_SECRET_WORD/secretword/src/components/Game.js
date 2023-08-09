ae81b1428d764f981ee2add55e43a48d1eeef0cd
Leonardo Rimes <rimesleo@gmail.com>
2023-08-09 10:16:57 -0300

    trabalhando no input do jogo e utilizando useref

--- 6_SECRET_WORD/secretword/src/components/Game.js
+++ 6_SECRET_WORD/secretword/src/components/Game.js
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import "./Game.css";
 
 const Game = ({
@@ -8,8 +9,21 @@ const Game = ({
   guessedLetters,
   wrongLetters,
   guesses,
-  score
+  score,
 }) => {
+  const [letter, setLetter] = useState("")
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    verifyLetter(letter)
+    setLetter("")
+
+    letterInputRef.current.focus()
+
+  }
+
   return (
     <div className="game">
       <p className="points">
@@ -21,18 +35,31 @@ const Game = ({
       </h3>
       <p> Você ainda tem {guesses} tentativas</p>
       <div className="wordContainer">
-       {letters.map((letter, i) => (
+        {letters.map((letter, i) =>
           guessedLetters.includes(letter) ? (
-            <span key={i} className="letter"> {letter} </span>
+            <span key={i} className="letter">
+              {" "}
+              {letter}{" "}
+            </span>
           ) : (
-            <span key={i} className="blankSquare"> </span>
+            <span key={i} className="blankSquare">
+              {" "}
+            </span>
           )
-       ))}
+        )}
       </div>
       <div className="letterContainer">
         <p> Tente advinhar uma letra da palavra: </p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
           <button> Jogar! </button>
         </form>
       </div>
