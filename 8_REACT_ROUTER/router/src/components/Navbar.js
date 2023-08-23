efd3774936eb42c605315975b4fe13eb40e9ff1f
Leonardo Rimes <rimesleo@gmail.com>
2023-08-23 11:16:45 -0300

    Finalizando módulo sobre rotas

--- 8_REACT_ROUTER/router/src/components/Navbar.js
+++ 8_REACT_ROUTER/router/src/components/Navbar.js
@@ -1,13 +1,14 @@
-
+// 2 - links com reacts router
 import "./Navbar.css"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/about"> Sobre </Link>
-
+        {/* <Link to="/"> Home </Link>
+        <Link to="/about"> Sobre </Link> */}
+        <NavLink to="/" /*className={({isActive}) => (isActive ? 'está ativo' : "nao-ativo")}*/>Home</NavLink>
+        <NavLink to="/about" >Sobre</NavLink>
         
     </nav>
   )
