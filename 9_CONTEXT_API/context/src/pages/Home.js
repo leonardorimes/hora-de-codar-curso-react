0ca5205c63a8d6a5b71b6e3a2914863c6e69d15e
Leonardo Rimes <rimesleo@gmail.com>
2023-08-25 07:57:38 -0300

    Consumindo contexto

--- 9_CONTEXT_API/context/src/pages/Home.js
+++ 9_CONTEXT_API/context/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useContext } from 'react'
+import {CounterContext} from '../context/CounterContext'
 
 import "./Home.css"
 
 const Home = () => {
+  const {counter} = useContext(CounterContext)
+
   return (
     <div>
-      Home
+     <h1> Home </h1> 
+     <p> Valor do contador: {counter}</p>
     </div>
   )
 }
