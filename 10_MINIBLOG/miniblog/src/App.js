d49da8d575b521c34aea37c7507c5784bf679eab
Leonardo Rimes <rimesleo@gmail.com>
2023-08-28 11:03:08 -0300

    criando nav e rodapé

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// pages
+import Home from './pages/Home/Home';
+import About from './pages/About/about';
+
+
+
 function App() {
   return (
     <div className="App">
-      <h1> MiniBlog </h1>
+      <BrowserRouter>
+      <Navbar />
+        <div className="container">
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+
+          </Routes>
+
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
