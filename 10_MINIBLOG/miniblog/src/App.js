7d2c46c2b4b13b0eff580798e3cd94ef63b3a0b1
Leonardo Rimes <rimesleo@gmail.com>
2023-09-03 10:50:59 -0300

    bloqueando páginas que precisam de autentificacao

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,68 +1,73 @@
-import './App.css';
+import "./App.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
 // hooks
-import { useState, useEffect } from 'react'
-import { useAuthentication } from './hooks/useAuthentication';
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
-// context 
-import { AuthProvider } from './context/AuthContext';
+// context
+import { AuthProvider } from "./context/AuthContext";
 
 // components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // pages
-import Home from './pages/Home/Home';
-import About from './pages/About/about';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import CreatePost from './pages/CreatePost/CreatePost';
-import Dashboard from './pages/Dashboard/Dashboard';
-
-
+import Home from "./pages/Home/Home";
+import About from "./pages/About/about";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
 
-  const [user, setUser] = useState(undefined)
-  const {auth} = useAuthentication()
-
-  const loadingUser = user ===undefined
+  const loadingUser = user === undefined;
 
   useEffect(() => {
-
     onAuthStateChanged(auth, (user) => {
-      setUser(user)
-    })
+      setUser(user);
+    });
+  }, [auth]);
 
-  }, [auth])
-
-  if(loadingUser){
-    return <p> Carregando... </p>
+  if (loadingUser) {
+    return <p> Carregando... </p>;
   }
-
 
   return (
     <div className="App">
-     <AuthProvider value={{user}}>
-     <BrowserRouter>
-      <Navbar />
-        <div className="container">
-          <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path="/posts/create" element={<CreatePost/>} />
-              <Route path="/dashboard" element={<Dashboard/>}/>
-          </Routes>
-
-        </div>
-        <Footer />
-      </BrowserRouter>
-     </AuthProvider>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
