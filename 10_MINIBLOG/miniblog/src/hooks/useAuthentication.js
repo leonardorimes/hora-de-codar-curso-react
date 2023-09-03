8ffc8d3cd95b1d0aadee1ff1fe411649596b4850
Leonardo Rimes <rimesleo@gmail.com>
2023-09-02 21:03:02 -0300

    adicionando usuário no sistema

--- 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
+++ 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
@@ -1,3 +1,5 @@
+import {db} from "../firebase/config"
+
 import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -15,5 +17,71 @@ export const useAuthentication = () => {
     // cleanup
     // deal with memory leak
     const [cancelled, setCancelled] = useState(false)
+
+    const auth = getAuth()
+
+
+    function checkIfIsCancelled(){
+        if(cancelled){
+            return
+        }
+    }
+
+    const createUser = async (data) => {
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(null)
+
+        try{
+
+            const { user } = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            )
+
+
+         await updateProfile(user, {
+            displayName: data.displayName
+         })
+
+         setLoading(false)
+         return user
+
+
+        } catch(error) {
+            console.log(error.message)
+            console.log(typeof error.message)
+
+            let systemErrorMessage
+
+            if(error.message.includes("Password")){
+                systemErrorMessage = "A senha precisa conter ao menos 6 caracteres"
+            } else if(error.message.includes("email-already")){
+                systemErrorMessage = "Email já cadastrado"
+            } else{
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde"
+            }
+
+            setLoading(false)
+
+            setError(systemErrorMessage)
+        }
+
+        
+    }
+
+    useEffect(() => {
+        return () => setCancelled(true)
+
+    }, [])
+
+    return{
+        auth,
+        createUser,
+        error,
+        loading
+    }
 
 }
--- 10_MINIBLOG/miniblog/src/pages/Register/Register.js
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.js
@@ -1,34 +1,42 @@
+import { useAuthentication } from "../../hooks/useAuthentication";
 import styles from "./Register.module.css";
 
 import { useState, useEffect } from "react";
 
-
-
 const Register = () => {
-  const [displayName, setDisplayName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
-  const [error, setError] = useState("")
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setError()
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError();
 
     const user = {
       displayName,
       email,
-      password
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais");
+      return;
     }
 
-    if(password !== confirmPassword){
-      setError("As senhas precisam ser iguais")
-      return
+    const res = await createUser(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
     }
-
-    console.log(user)
-
-  }
+  }, [authError]);
 
   return (
     <div className={styles.register}>
@@ -83,9 +91,12 @@ const Register = () => {
           />
         </label>
 
-        <button className="btn">
-          Cadastrar
-        </button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
         {error && <p className="error"> {error} </p>}
       </form>
     </div>
