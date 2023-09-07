ee74ef91d038fa442c5dc0dc7b568de6f93ad3aa
Leonardo Rimes <rimesleo@gmail.com>
2023-09-07 20:45:51 -0300

    aprimotando o search

--- 10_MINIBLOG/miniblog/src/pages/Search/Search.js
+++ 10_MINIBLOG/miniblog/src/pages/Search/Search.js
@@ -1,17 +1,38 @@
+import styles from "./Search.module.css"
 import React from 'react'
 
 //hooks
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import useQuery from "../../hooks/useQuery"
 
+// components
+import PostDetail from "../../components/PostDetail"
+import { Link } from 'react-router-dom'
+
+
 const Search = () => {
     const query = useQuery()
     const search = query.get("q")
 
+    const {documents: posts} = useFetchDocuments("posts", search)
+
   return (
-    <div>
-    {console.log(search)}
-     <p>{search}</p>
+    <div className={styles.search_container}>
+      <div>
+        <h2> Search </h2>
+        <div>
+
+          {posts && posts.length === 0 && (
+            <div className={styles.noposts}>
+              <p>Não foram encontrados posts a partir de sua busca</p>
+              <Link to="/" className="btn btn-dark"> Voltar </Link>
+            </div>
+          )}
+          {posts && posts.map((post) => (
+            <PostDetail key={post.id} post={post}></PostDetail>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
