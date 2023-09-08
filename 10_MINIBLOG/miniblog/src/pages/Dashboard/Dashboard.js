4f10f9e5789369fb88c9415efa37cbcbddc071d3
Leonardo Rimes <rimesleo@gmail.com>
2023-09-08 17:15:21 -0300

    exibindo posts do usuario e concluindo estrutura da dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,35 +1,64 @@
-import React from './Dashboard.module.css'
+import React from "./Dashboard.module.css";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 //hooks
-import {useAuthValue} from "../../context/AuthContext"
-import {useFetchDocuments} from "../../hooks/useFetchDocuments"
-
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  const {user} = useAuthValue()
-  const uid = user.uid
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-  // posts do usuário
-  const posts = []
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {};
+
+  if(loading) {
+    return <p> Carregando...</p>
+  }
 
   return (
     <div>
-       <h2>Dashboard</h2>
-       <p>Gerencie os seus posts</p>
-        {posts && posts.length === 0 ? (
-          <div className={StyleSheet.noposts}>
-            <p>Não foram encontrados posts</p>
-            <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
-          </div>
-        ) : (
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={StyleSheet.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <>
           <div>
-              <p>Tem posts!</p>
+            <span> Título </span>
+            <span> Ações </span>
           </div>
-        )}
-    </div>
-  )
-}
 
-export default Dashboard
+          {posts && posts.map((post) => (
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                  Ver
+                </Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                  Editar
+                </Link>
+                <button
+                  onClick={() => deleteDocument(post.id)}
+                  className="btn btn-outline btn-danger"
+                >
+                  Excluir
+                </button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
