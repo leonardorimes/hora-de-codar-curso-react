220424eafc61f446be5223aba75e6e35e9f4d4f8
Leonardo Rimes <rimesleo@gmail.com>
2023-09-10 18:17:40 -0300

    finalizando pagina de edição

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
-import React from "./Dashboard.module.css";
+import styles from "./Dashboard.module.css";
 
 import { Link } from "react-router-dom";
 
 //hooks
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
@@ -12,14 +13,16 @@ const Dashboard = () => {
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
-  const deleteDocument = (id) => {};
+  const {deleteDocument} = useDeleteDocument("posts")
+
+ 
 
   if(loading) {
     return <p> Carregando...</p>
   }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -31,13 +34,13 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={styles.post_header}>
             <span> Título </span>
             <span> Ações </span>
           </div>
 
           {posts && posts.map((post) => (
-            <div key={post.id}>
+            <div key={post.id} className={styles.post_row}> 
               <p>{post.title}</p>
               <div>
                 <Link to={`/posts/${post.id}`} className="btn btn-outline">
