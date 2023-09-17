061cd2a77f0407eb6356cfbe09cbb48389a5e88b
Leonardo Rimes <rimesleo@gmail.com>
2023-09-17 14:32:04 -0300

    terminando o useEffect

---
+++ 11_HOOKS/reacthooks/src/components/HookUseState.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const HookUseState = () => {
+  // 1 - useState
+  let userName = "João";
+
+  const [name, setName] = useState("Leonardo");
+
+  const changeNames = () => {
+    userName = "João Souza";
+    setName("Leonardo Rimes");
+
+    console.log(name);
+    console.log(userName);
+  };
+
+  // 2 - useState e input
+
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    //envio a uma api
+    console.log(age)
+  }
+
+  return (
+    <div>
+      {/* 1 - useState */}
+      <h2>useState</h2>
+      <p> Variável: {userName}</p>
+      <p> userState: {name}</p>
+      <button onClick={changeNames}> mudar nomes </button>
+      {/* 2 - useState e input */}
+      <p>Digite a sua idade</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      <p>Você tem {age} anos</p>
+      <hr />
+    </div>
+  );
+};
+
+export default HookUseState;
---
+++ 11_HOOKS/reacthooks/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HookUseReducer from '../components/HookUseReducer'
+import HookUseState from '../components/HookUseState'
+import HookUseEffect from '../components/HookUseEffect'
+
+const Home = () => {
+  return (
+    <div>
+        
+        <HookUseState />
+        <HookUseReducer />
+        <HookUseEffect />
+
+    </div>
+  )
+}
+
+export default Home
