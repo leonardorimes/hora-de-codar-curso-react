199d574249cd492af906c960017aa65764b07e15
Leonardo Rimes <rimesleo@gmail.com>
2023-09-21 08:12:08 -0300

    criando schemas e controller do usuário

--- 12-REACTGRAM/backend/routes/Router.js
+++ 12-REACTGRAM/backend/routes/Router.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express()
 
+
+
+router.use("/api/users", require("./UserRoutes"))
+
 // test route
 router.get("/", (req, res) => {
     res.send("API WORKING !")
