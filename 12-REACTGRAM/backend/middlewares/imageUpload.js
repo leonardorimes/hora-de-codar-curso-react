9f047f6b4a923d9dd63ed440fcbb3a63647cc92f
Leonardo Rimes <rimesleo@gmail.com>
2023-09-24 12:05:16 -0300

    atualizando o usuario

--- 12-REACTGRAM/backend/middlewares/imageUpload.js
+++ 12-REACTGRAM/backend/middlewares/imageUpload.js
@@ -23,7 +23,7 @@ const imageStorage = multer.diskStorage({
 const imageUpload = multer({
     storage: imageStorage,
     fileFilter(req, file, cb) {
-        if(!file.originalname.match(/\.(png | jpg)$/)){
+        if(!file.originalname.match(/\.((png|jpg|PNG|JPG))$/)){
             // upload only png and jpg format
             return cb(new Error("Por favor, envie apenas png ou jpg!"))
         }
