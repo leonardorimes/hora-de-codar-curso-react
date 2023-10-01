ee8d20ed8ca2dd95e944b8df87944affcafebb6a
Leonardo Rimes <rimesleo@gmail.com>
2023-10-01 15:01:31 -0300

    finalizando login

--- 12-REACTGRAM/frontend/src/pages/Auth/Login.js
+++ 12-REACTGRAM/frontend/src/pages/Auth/Login.js
@@ -6,17 +6,35 @@ import Message from "../../components/Message"
 
 // Hooks
 import { useEffect, useState } from "react"
-import { UseSelector, useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 
 // Redux
+import {login, reset} from '../../slices/authSlice'
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const dispatch = useDispatch()
+
+  const {loading, error} = useSelector((state) => state.auth)
+
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+
+    const user = {
+      email,
+      password
+    }
+
+    dispatch(login(user))
   };
+
+  // clean all auth states
+  useEffect(() => {
+      dispatch(reset())
+  }, [dispatch])
+
 
   return (
     <div id="login">
@@ -35,7 +53,9 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password || ""}
         />
-        <input type="submit" value="Entrar" />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
       </form>
       <p>
         Não tem uma conta? <Link to="/register">Clique aqui</Link>
--- 12-REACTGRAM/frontend/src/services/authService.js
+++ 12-REACTGRAM/frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import { api, requestConfig } from "../utils/config";
 
-// Register an user
+// Register a user
 const register = async (data) => {
   const config = requestConfig("POST", data);
 
@@ -19,38 +19,36 @@ const register = async (data) => {
   }
 };
 
-// Logout an user
+// Logout a user
 const logout = () => {
-  localStorage.removeItem("user")
-}
+  localStorage.removeItem("user");
+};
 
-// Sign in an user
-const login = async(data) => {
+// Sign in a user
+const login = async (data) => {
+  const config = requestConfig("POST", data);
 
-  const config = requestConfig("POST", data)
+  try {
+    const res = await fetch(api + "/users/login", config)
+      .then((res) => res.json())
+      .catch((err) => err)
 
-  try{
+      console.log(res)
 
-    const res = await fetch(api + "/users/login", config )
-                  .then((res) => res.json)
-                  .catch((err) => err)
+    if (res._id) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
 
-    if(res){
-        localStorage.setItem("user", JSON.stringify(res))
-    }  
-    
-    return res
-
-  }catch(error){
-    console.log(error)
+    return res;
+  } catch (error) {
+    console.log(error);
   }
-}
-
+};
 
 const authService = {
   register,
   logout,
-  login
+  login,
 };
 
 export default authService;
