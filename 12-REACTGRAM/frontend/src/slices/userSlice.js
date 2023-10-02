1051e7e37fdf139013efd4e82cad2e8cd6f5e3a2
Leonardo Rimes <rimesleo@gmail.com>
2023-10-02 10:14:15 -0300

    slice de perfil de usuário

--- 12-REACTGRAM/frontend/src/slices/userSlice.js
+++ 12-REACTGRAM/frontend/src/slices/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import userService from "../services/userService"
 
 const initialState = {
     user: {},
@@ -8,7 +9,19 @@ const initialState = {
     message: null
 }
 
-// funcoes
+// Get user details
+export const profile = createAsyncThunk(
+    "user/profile",
+    async(user, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await userService.profile(user, token)
+
+        return data
+
+    }
+)
 
 export const userSlice = createSlice({
     name: "user",
@@ -16,8 +29,21 @@ export const userSlice = createSlice({
     reducers: {
         resetMessage: (state) => {
             state.message = null
-
         }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(profile.pending, (state) => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(profile.fulfilled, (state, action) => {
+          state.loading = false;
+          state.success = true;
+          state.error = null;
+          state.user = action.payload;
+        })
+        
     }
 })
 
