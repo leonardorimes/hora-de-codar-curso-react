0935b09b6eb1564e8102e68f6fe7226b4c6821f6
Leonardo Rimes <rimesleo@gmail.com>
2023-10-06 12:57:15 -0300

    Slice de inserção de texto

--- 12-REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12-REACTGRAM/frontend/src/slices/photoSlice.js
@@ -10,6 +10,24 @@ const initialState = {
     message: null
 }
 
+// Publish user photo
+export const publishPhoto = createAsyncThunk(
+    "photo/publish",
+    async(photo, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.publishPhoto(photo, token)
+
+        // check for errors
+        if(data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
 // funcoes
 
 export const photoSlice = createSlice({
@@ -19,6 +37,26 @@ export const photoSlice = createSlice({
         resetMessage: (state) => {
             state.message = null
         }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(publishPhoto.pending, (state) => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(publishPhoto.fulfilled, (state, action) => {
+          state.loading = false;
+          state.success = true;
+          state.error = null;
+          state.photo = action.payload;
+          state.photos.unshift(state.photo)
+          state.message = "Foto publicada com sucesso"
+        })
+        .addCase(publishPhoto.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = {};
+          })
     }
 })
 
