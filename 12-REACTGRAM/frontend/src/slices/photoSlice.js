c8cd84c5b058b82a274916b559958fe2e6d799eb
Leonardo Rimes <rimesleo@gmail.com>
2023-10-10 07:27:04 -0300

    slice de comentario

--- 12-REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12-REACTGRAM/frontend/src/slices/photoSlice.js
@@ -101,6 +101,27 @@ export const like = createAsyncThunk("photo/like", async (id, thunkAPI) => {
   return data;
 });
 
+// add comment to a photo
+export const comment = createAsyncThunk(
+  "photo/comment",
+  async (photoData, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await photoService.comment(
+      { comment: photoData.comment },
+      photoData.id,
+      token
+    );
+
+    // check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+    return data;
+  }
+
+    
+);
+
 // funcoes
 
 export const photoSlice = createSlice({
@@ -217,6 +238,19 @@ export const photoSlice = createSlice({
         state.message = action.payload.message;
       })
       .addCase(like.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(comment.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        state.photo.comments.push(action.payload.comment)
+
+        state.message = action.payload.message;
+      })
+      .addCase(comment.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
       });
