034e4853d0064a779b7639164e0a7065759ebe6c
Leonardo Rimes <rimesleo@gmail.com>
2023-10-12 13:05:14 -0300

    criando pagina de busca

--- 12-REACTGRAM/frontend/src/App.js
+++ 12-REACTGRAM/frontend/src/App.js
@@ -17,6 +17,7 @@ import Register from "./pages/Auth/Register"
 import EditProfile from "./pages/EditProfile/EditProfile"
 import Profile from "./pages/Profile/Profile";
 import Photo from "./pages/Photo/Photo";
+import Search from "./pages/Search/Search";
 
 function App() {
   const { auth, loading } = useAuth();
@@ -51,6 +52,12 @@ function App() {
               path="/register"
               element={!auth ? <Register /> : <Navigate to="/" />}
             />
+
+            <Route
+              path="/search"
+              element={auth ? <Search /> : <Navigate to="/login" />}
+            />
+
              <Route
               path="/photos/:id"
               element={auth ? <Photo /> : <Navigate to="/login" />}
---
+++ 12-REACTGRAM/frontend/src/pages/Search/Search.js
@@ -0,0 +1,20 @@
+import "./Search.css"
+
+// hooks
+
+import { useEffect } from "react"
+import { UseSelector, useDispatch } from "react-redux"
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage"
+
+// components
+import LikeContainer from "../../components/LikeContainer"
+import PhotoItem from "../../components/PhotoItem"
+import {Link} from "react-router-dom"
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
