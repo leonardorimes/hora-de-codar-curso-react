f28939734e7370e9bed9cee44a6ba442374bd8e4
Leonardo Rimes <rimesleo@gmail.com>
2023-10-16 10:10:13 -0300

    finalizando apis

--- 12-REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12-REACTGRAM/frontend/src/slices/photoSlice.js
@@ -118,18 +118,29 @@ export const comment = createAsyncThunk(
     }
     return data;
   }
-
-    
 );
 
 // Get all photos
-export const getPhotos = createAsyncThunk("photo/getall", async(_,thunkAPI) => {
+export const getPhotos = createAsyncThunk(
+  "photo/getall",
+  async (_, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await photoService.getPhotos(token);
 
-  const token = thunkAPI.getState().auth.user.token;
-  const data = await photoService.getPhotos(token)
+    return data;
+  }
+);
 
-  return data
-})
+// Search photo by title
+export const searchPhotos = createAsyncThunk(
+  "photo/search",
+  async (query, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await photoService.searchPhotos(query, token);
+
+    return data;
+  }
+);
 
 // funcoes
 
@@ -255,7 +266,7 @@ export const photoSlice = createSlice({
         state.success = true;
         state.error = null;
 
-        state.photo.comments.push(action.payload.comment)
+        state.photo.comments.push(action.payload.comment);
 
         state.message = action.payload.message;
       })
@@ -268,6 +279,16 @@ export const photoSlice = createSlice({
         state.error = null;
       })
       .addCase(getPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
+      })
+      .addCase(searchPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(searchPhotos.fulfilled, (state, action) => {
         state.loading = false;
         state.success = true;
         state.error = null;
