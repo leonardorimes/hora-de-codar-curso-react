9d41da2129b8a98e325a862fe1a51a18a8cdb948
Leonardo Rimes <rimesleo@gmail.com>
2023-10-16 10:42:28 -0300

    setup do typescript

---
+++ extra3-setupTypescript/index.ts
@@ -0,0 +1,13 @@
+const firstName:string = "Leonardo"
+const anotherName:number = 1
+const x:boolean = true
+
+function greeting(name: string) {
+    console.log("Olá " + name)
+}
+
+
+greeting(firstName)
+// greeting(anotherName)
+// greeting(x)
+
