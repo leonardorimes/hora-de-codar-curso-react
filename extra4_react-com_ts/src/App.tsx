0b149a2a80de3d4f461fdc176d4837da506fd647
Leonardo Rimes <rimesleo@gmail.com>
2023-10-18 08:15:28 -0300

    adicionando contexto

--- extra4_react-com_ts/src/App.tsx
+++ extra4_react-com_ts/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {createContext} from "react";
 
 // 4 - importação de componente
 import FirstComponent from "./components/firstComponent";
@@ -9,19 +9,50 @@ import Destructuring, { Category } from "./components/Destructuring";
 
 // 6 - useState
 import State from "./components/State";
+import Context from "./components/Context";
+
+
+// 8 - type
+type textOrNull = string | null;
+type fixed = "Isso" | "Ou" | "Aquilo"
+
+// 9 - context
+interface IAppContext {
+  language: string
+  framework: string
+  projects: number
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
 
 function App() {
   // 1 - Variaveis
-  const name: string = "Leonardo";
-  const age: number = 31;
-  const isWorking: boolean = true;
+  const name: string = "Leonardo"
+  const age: number = 31
+  const isWorking: boolean = true
 
   // 2 - funções
   const userGreeting = (name: string): string => {
-    return `Olá, ${name}!`;
+    return `Olá, ${name}!`
   };
 
+  // 8- type
+  const myText: textOrNull = "Tem algum texto aqui"
+  let mySecondText: textOrNull = null
+
+  mySecondText = "Opa"
+
+  const testandoFixed:fixed = "Isso"
+
+  // 9 - Context
+  const contextValue:IAppContext = {
+    language: "JavaScript",
+    framework: "express",
+    projects: 5
+  }
+
   return (
+    <AppContext.Provider value={contextValue}>
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
@@ -46,8 +77,11 @@ function App() {
       />
 
       <State />
+      {myText && <p> Tem texto na variável </p>}
+      {mySecondText && <p>Tem texto na variável</p>}
     </div>
-    
+    <Context />
+    </AppContext.Provider>
   );
 }
 
---
+++ extra4_react-com_ts/src/components/Context.tsx
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react'
+
+import { AppContext } from '../App'
+
+const Context = () => {
+
+    const details = useContext(AppContext)
+
+  return (
+    <div>
+        {details && (
+            <div>
+                <h2>Linguagem: {details.language}</h2>
+                <h4>FW: {details.framework}</h4>
+                <p>QTD: {details.projects}</p>
+
+
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default Context
