79fcd674bf734443a26e948c46eb8119a026c011
Leonardo Rimes <rimesleo@gmail.com>
2023-10-21 17:41:30 -0300

    finalizando update

--- extra5-reactts-todo/extra5-reactts-todo/src/App.tsx
+++ extra5-reactts-todo/extra5-reactts-todo/src/App.tsx
@@ -38,6 +38,18 @@ function App() {
     setTaskToUpdate(task);
   };
 
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  };
+
   return (
     <div className="App">
       <div>
@@ -47,6 +59,7 @@ function App() {
               btnText="Editar Tarefa"
               taskList={taskList}
               task={taskToUpdate}
+              handleUpdate={updateTask}
             />
           }
         />
