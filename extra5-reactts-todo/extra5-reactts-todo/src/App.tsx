d572ca8b219b47a156f835e4b513b9baf699fb9c
Leonardo Rimes <rimesleo@gmail.com>
2023-10-21 17:08:56 -0300

    evento para fechar o modal

--- extra5-reactts-todo/extra5-reactts-todo/src/App.tsx
+++ extra5-reactts-todo/extra5-reactts-todo/src/App.tsx
@@ -23,10 +23,25 @@ function App() {
     );
   };
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (): void => {
+    hideOrShowModal(true);
+  };
+
   return (
     <div className="App">
       <div>
-        <Modal />
+        <Modal
+          children={<TaskForm btnText="Editar Tarefa" taskList={taskList} />}
+        />
         <Header />
         <main className={styles.main}>
           <div>
@@ -39,7 +54,11 @@ function App() {
           </div>
           <div>
             <h2>Suas tarefas: </h2>
-            <TaskList taskList={taskList} handleDelete={deleteTask} />
+            <TaskList
+              taskList={taskList}
+              handleDelete={deleteTask}
+              handleEdit={editTask}
+            />
           </div>
         </main>
         <Footer />
--- extra5-reactts-todo/extra5-reactts-todo/src/components/Modal.tsx
+++ extra5-reactts-todo/extra5-reactts-todo/src/components/Modal.tsx
@@ -3,13 +3,23 @@ import React from "react";
 // CSS
 import styles from "./Modal.module.css";
 
-type Props = {};
+type Props = {
+  children: React.ReactNode;
+};
 
-const Modal = (props: Props) => {
+const Modal = ({ children }: Props) => {
+  const closeModal = (e: React.MouseEvent): void => {
+    const modal = document.querySelector("#modal");
+    modal!.classList.add("hide");
+  };
+
   return (
-    <div id="modal">
-      <div></div>
-      <h2> Texto modal </h2>
+    <div id="modal" className="hide">
+      <div className={styles.fade} onClick={closeModal}></div>
+      <div className={styles.modal}>
+        <h2> Texto modal </h2>
+        {children}
+      </div>
     </div>
   );
 };
