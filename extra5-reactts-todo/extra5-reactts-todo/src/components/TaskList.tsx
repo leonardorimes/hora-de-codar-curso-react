86a79412054f8c54b6ad4bdb826ba04571b33bee
Leonardo Rimes <rimesleo@gmail.com>
2023-10-21 15:12:57 -0300

    html dos itens da lista

--- extra5-reactts-todo/extra5-reactts-todo/src/components/TaskList.tsx
+++ extra5-reactts-todo/extra5-reactts-todo/src/components/TaskList.tsx
@@ -16,7 +16,14 @@ const TaskList = ({ taskList }: Props) => {
       {taskList.length > 0 ? (
         taskList.map((task) => (
           <div key={task.id}>
-            <p> {task.title}</p>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
           </div>
         ))
       ) : (
